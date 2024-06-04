0e4fa7374f7c5bf8e4a88c33327d751582badbf6
Тетяна <cerryz.ta.ha@gmail.com>
2024-06-04 15:58:26 +0300

    add post method

--- src/test/java/api/ApiDeliveryTest.java
+++ src/test/java/api/ApiDeliveryTest.java
@@ -1,7 +1,9 @@
 package api;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ApiDeliveryTest {
@@ -49,4 +51,105 @@ public class ApiDeliveryTest {
                 .assertThat()
                 .statusCode(HttpStatus.SC_BAD_REQUEST);
     }
+
+    @Test
+    public void checkOrderDetailsWithCorrectOrderIdAndCheckOrderStatus() {
+        String receiveOrderStatus = RestAssured
+                .given()
+                .log()
+                .all()
+                .get(BASE_URL + BASE_PATH + "4")
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .extract()
+                .path("status");
+
+        Assertions.assertEquals("OPEN", receiveOrderStatus);
+    }
+
+    @Test
+    public void checkOrderIdInResponse() {
+        int orderIdRequested = 4;
+        int receivedOrderID = RestAssured
+                .given()
+                .log()
+                .all()
+                .get(BASE_URL + BASE_PATH + orderIdRequested)
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .extract()
+                .path("id");
+
+        Assertions.assertEquals(orderIdRequested, receivedOrderID);
+    }
+
+    @Test
+    public void checkOrderCreation() {
+        String requestBodyUglyWay = "{\n" +
+                "  \"status\": \"OPEN\",\n" +
+                "  \"courierId\": 0,\n" +
+                "  \"customerName\": \"Tata\",\n" +
+                "  \"customerPhone\": \"11223344\",\n" +
+                "  \"comment\": \"hello\",\n" +
+                "  \"id\": 0\n" +
+                "} ";
+
+        String receivedStatus = RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                //.queryParam("username", "Olga")
+                //.queryParam("password", "123456")
+                .body(requestBodyUglyWay)
+                .log()
+                .all()
+                .post(BASE_URL + BASE_PATH)
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .extract()
+                .path("status");
+        Assertions.assertEquals("OPEN", receivedStatus);
+    }
+
+    @Test
+    public void createOrderWithIncorrectStatusAndCheckResponseMessage() {
+        String requestBodyUglyWay = "{\n" +
+                "  \"status\": \"CLOSED\",\n" +
+                "  \"courierId\": 0,\n" +
+                "  \"customerName\": \"Tata\",\n" +
+                "  \"customerPhone\": \"11223344\",\n" +
+                "  \"comment\": \"hello\",\n" +
+                "  \"id\": 0\n" +
+                "} ";
+
+        String responseBody = RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body(requestBodyUglyWay)
+                .log()
+                .all()
+                .post(BASE_URL + BASE_PATH)
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .and()
+                .extract()
+                .asString();
+        Assertions.assertEquals("Incorrect query", responseBody);
+    }
+
+
 }
